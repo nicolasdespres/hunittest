e0d1c88b8bc37160b93f8a99ee89702a153e803f
Nicolas Despres <nicolas.despres@gmail.com>
2015-03-09 17:57:35 +0100

    Add more example in testtest/

---
+++ testtest/test_allgood.py
@@ -0,0 +1,87 @@
+# -*- encoding: utf-8 -*-
+"""
+
+"""
+
+import unittest
+import time
+
+DELAY = 0.25
+
+class Case1(unittest.TestCase):
+
+    def test_success(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success1(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success2(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success3(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success4(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success5(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+class Case2(unittest.TestCase):
+
+    def test_success(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success1(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success2(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success3(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success4(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success5(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+class Case3(unittest.TestCase):
+
+    def test_success(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success1(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success2(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success3(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success4(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)
+
+    def test_success5(self):
+        time.sleep(DELAY)
+        self.assertEqual(1, 1)

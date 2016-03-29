a5ce88f8b57de3613efd7e97b2afb264b527afc0
Nicolas Despres <nicolas.despres@gmail.com>
2016-03-29 21:08:27 +0200

    Allow to collect only top level test specs.

--- hunittest/collectlib.py
+++ hunittest/collectlib.py
@@ -43,13 +43,16 @@ def build_test_name(test_module, test_case=None, test_method=None):
             return x
     return pyname_join(map(transform, l))
 
-def collect_all_test_modules(package, pattern):
+def collect_all_test_modules(package, pattern, top_level_only=False):
     # We cannot use the finder to load the module because it mess up unittest.
     # Using import_module() is fine.
     for _, name, ispkg in pkgutil.walk_packages(package.__path__,
                                                 package.__name__+'.'):
         if not ispkg and re.match(pattern, re.sub(r"^.*\.", "", name)):
-            yield import_module(name)
+            if top_level_only:
+                yield name
+            else:
+                yield import_module(name)
 
 class TestSpecType(Enum):
     package = 1
@@ -148,25 +151,40 @@ def collect_all_from_test_case(test_case):
     for test_method in collect_test_names(test_case):
         yield build_test_name(test_case.__module__, test_case, test_method)
 
-def collect_all_from_module(test_module):
-    for test_case in collect_test_cases(test_module):
-        yield from collect_all_from_test_case(test_case)
+def collect_all_from_module(test_module, top_level_only=False):
+    if top_level_only:
+        yield build_test_name(test_module)
+    else:
+        for test_case in collect_test_cases(test_module):
+            yield from collect_all_from_test_case(test_case)
 
-def collect_all_from_package(package, pattern):
-    for test_module in collect_all_test_modules(package, pattern):
-        yield from collect_all_from_module(test_module)
+def collect_all_from_package(package, pattern, top_level_only=False):
+    for test_module in collect_all_test_modules(package, pattern,
+                                                top_level_only=top_level_only):
+        yield from collect_all_from_module(test_module,
+                                           top_level_only=top_level_only)
 
-def collect_all(test_specs, pattern, top_level_directory):
+def collect_all(test_specs, pattern, top_level_directory, top_level_only=False):
+    """Collect all test case from the given test specification.
+
+    The top_level_only flag tells whether to load only the top level test
+    specs (i.e. packages and module). When this flag is on no module are loaded
+    and thus, the collection process should be faster.
+    """
     for test_spec in test_specs:
         tst, value = get_test_spec_type(test_spec, top_level_directory)
         if tst is TestSpecType.package:
-            yield from collect_all_from_package(value, pattern)
+            yield from collect_all_from_package(value, pattern,
+                                                top_level_only=top_level_only)
         elif tst is TestSpecType.module:
-            yield from collect_all_from_module(value)
+            yield from collect_all_from_module(value,
+                                               top_level_only=top_level_only)
         elif tst is TestSpecType.test_case:
-            yield from collect_all_from_test_case(value)
+            if not top_level_only:
+                yield from collect_all_from_test_case(value)
         elif tst is TestSpecType.test_method:
-            yield pyname_join((value.__module__, value.__qualname__))
+            if not top_level_only:
+                yield pyname_join((value.__module__, value.__qualname__))
         else:
             raise RuntimeError("unsupported test spec type: {}"
                                .format(tst))
